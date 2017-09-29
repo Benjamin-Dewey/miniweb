a43aa009ca13779821a12b006c20656cbf966208
Benjamin Dewey <benjamin.m.dewey@gmail.com>
2017-09-29 16:36:14 -0400

    finish part 1

--- src/warmUp.js
+++ src/warmUp.js
@@ -1,1 +1,18 @@
 // warmUp.js
+
+const net = require('net');
+const HOST = '127.0.0.1';
+const PORT = 8080;
+
+const server = net.createServer(sock => {
+  sock.on('data', () => {
+    const newLine = '\r\n';
+    const statusLine = 'HTTP/1.1 200 OK';
+    const headerFields = 'Content-Type: text/html; charset=UTF-8';
+    const body = '<em>Hello</em> <strong>World</strong>';
+    const response = `${statusLine}${newLine}${headerFields}${newLine}${newLine}${body}`;
+    sock.end(response);
+  });
+});
+
+server.listen(PORT, HOST);
