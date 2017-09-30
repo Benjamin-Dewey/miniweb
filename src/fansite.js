4d3aef511064798fadbeca85fd5157b06b1aab2b
Benjamin Dewey <benjamin.m.dewey@gmail.com>
2017-09-30 12:10:16 -0400

    add images and stub files for fansite

--- src/fansite.js
+++ src/fansite.js
@@ -1,2 +1,14 @@
 // fansite.js
 // create your own fansite using your miniWeb framework
+
+const miniWebApp = require('./miniWeb.js').App;
+
+const fanSite = new miniWebApp();
+
+fanSite.get('/', (req, res) => res.sendFile('/html/index.html'));
+fanSite.get('/home', (req, res) => res.redirect(301, 'http://localhost:8080/'));
+fanSite.get('/image1.jpg', (req, res) => res.sendFile('/img/image1.jpg'));
+fanSite.get('/image2.png', (req, res) => res.sendFile('/img/image2.png'));
+fanSite.get('/image3.gif', (req, res) => res.sendFile('/img/image3.gif'));
+
+fanSite.listen(8080, '127.0.0.1');
