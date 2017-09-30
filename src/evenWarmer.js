0ee1a996eed51d1cc77dad50f08f3a89d2e52295
Benjamin Dewey <benjamin.m.dewey@gmail.com>
2017-09-29 20:33:50 -0400

    implement Response Object, passing tests

--- src/evenWarmer.js
+++ src/evenWarmer.js
@@ -27,7 +27,90 @@ class Request {
   toString() { return this.httpRequest; }
 }
 
-module.exports = { Request };
+class Response {
+  constructor(socket) {
+    this.sock = socket;
+    this.headers = {};
+    this.body = '';
+    this.statusCode = 0;
+
+    this.newLine = '\r\n';
+    this.http = 'HTTP/1.1';
+  }
+
+  getDescriptionForStatusCode(code) {
+    switch (true) {
+      case code >= 100 && code <= 199: // 1xx
+        return 'Received';
+      case code >= 200 && code <= 299: // 2xx
+        return 'OK';
+      case code >= 300 && code <= 399: // 3xx
+        return code === 302 ? 'Found' : code === 301 ? 'Moved Permanently' : 'See Other';
+      case code >= 400 && code <= 499: // 4xx
+        return code === 404 ? 'Not Found' : 'Bad Request';
+      case code >= 500 && code <= 599: // 5xx
+        return 'Internal Server Error';
+      default:
+        return 'None';
+    }
+  }
+
+  setHeader(name, value) { this.headers[name] = value; }
+
+  write(data) { this.sock.write(data); }
+
+  end(s) { this.sock.end(s); }
+
+  send(statusCode, body) {
+    this.statusCode = statusCode;
+    this.body = body;
+
+    const statusLine = `${this.http} ${statusCode} ${this.getDescriptionForStatusCode(statusCode)}`;
+    const headerLines = Object.keys(this.headers).reduce((str, key) => {
+      return `${str === '' ? '' : str + this.newLine}${key}: ${this.headers[key]}`;
+    }, '');
+    const response = `${statusLine}${this.newLine}${headerLines}${headerLines === '' ? '' : this.newLine}${this.newLine}${body}`;
+
+    this.end(response);
+  }
+
+  writeHead(statusCode) {
+    this.statusCode = statusCode;
+
+    const statusLine = `${this.http} ${statusCode} ${this.getDescriptionForStatusCode(statusCode)}`;
+    const headerLines = Object.keys(this.headers).reduce((str, key) => {
+      return `${str === '' ? '' : str + this.newLine}${key}: ${this.headers[key]}`;
+    }, '');
+    const response = `${statusLine}${this.newLine}${headerLines}`;
+
+    this.write(response);
+  }
+
+  redirect(...args) {
+    const [statusCode, url] = args.length === 1 ? [301, ...args] : args;
+
+    this.statusCode = statusCode;
+    this.setHeader('Location', url);
+
+    const statusLine = `${this.http} ${statusCode} ${this.getDescriptionForStatusCode(statusCode)}`;
+    const headerLines = Object.keys(this.headers).reduce((str, key) => {
+      return `${str === '' ? '' : str + this.newLine}${key}: ${this.headers[key]}`;
+    }, '');
+    const response = `${statusLine}${this.newLine}${headerLines}`;
+
+    this.end(response);
+  }
+
+  toString() {
+    const statusLine = `${this.http} ${this.statusCode} ${this.getDescriptionForStatusCode(this.statusCode)}`;
+    const headerLines = Object.keys(this.headers).reduce((str, key) => {
+      return `${str === '' ? '' : str + this.newLine}${key}: ${this.headers[key]}`;
+    }, '');
+    return `${statusLine}${this.newLine}${headerLines}${headerLines === '' ? '' : this.newLine}${this.newLine}${this.body}`;
+  }
+}
+
+module.exports = { Request, Response };
 
 //------------------------------------------------------------------------------
 
@@ -55,7 +138,7 @@ const server = net.createServer(sock => {
         body = 'h2 {color: red;}';
         break;
       default:
-        statusLine = 'HTTP/1.1 404 PAGE_NOT_FOUND';
+        statusLine = 'HTTP/1.1 404 Not Found';
         headerFields = 'Content-Type: text/plain; charset=UTF-8';
         body = 'uh oh... 404 page not found!';
         break;
