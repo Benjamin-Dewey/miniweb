4527014db55d861ac9a7f41b9c25e92705e5520d
Benjamin Dewey <benjamin.m.dewey@gmail.com>
2017-09-30 13:11:31 -0400

    finish webapp

--- src/fansite.js
+++ src/fansite.js
@@ -7,6 +7,16 @@ const fanSite = new miniWebApp();
 
 fanSite.get('/', (req, res) => res.sendFile('/html/index.html'));
 fanSite.get('/home', (req, res) => res.redirect(301, 'http://localhost:8080/'));
+fanSite.get('/about', (req, res) => res.sendFile('/html/about.html'));
+fanSite.get('/rando', (req, res) => res.sendFile('/html/rando.html'));
+
+fanSite.get('/randomImage', (req, res) => {
+  const paths = ['/img/image1.jpg', '/img/image2.png', '/img/image3.gif'];
+  const randomImagePath = paths[Math.floor(Math.random() * paths.length)];
+  res.sendFile(randomImagePath);
+});
+
+fanSite.get('/css/base.css', (req, res) => res.sendFile('/css/base.css'));
 fanSite.get('/image1.jpg', (req, res) => res.sendFile('/img/image1.jpg'));
 fanSite.get('/image2.png', (req, res) => res.sendFile('/img/image2.png'));
 fanSite.get('/image3.gif', (req, res) => res.sendFile('/img/image3.gif'));
